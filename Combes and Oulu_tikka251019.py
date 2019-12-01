6e9e85c566426206b855c66d873a6896e2674fba
paulitikka <30927365+paulitikka@users.noreply.github.com>
2019-12-01 11:30:05 +0100

    Update Combes and Oulu_tikka251019.py

--- Combes and Oulu_tikka251019.py
+++ Combes and Oulu_tikka251019.py
@@ -1000,6 +1000,46 @@ speed_NP_RND = np.mean(b1, axis=1)[80:180]*60
 #np.random.choice(np.concatenate([np.array(i) for i in b1*60]), 1001, False)
 #%%
 
+                                     
+#http://numpy-discussion.10968.n7.nabble.com/Rolling-window-moving-average-moving-std-and-more-td4744.html
+import numpy as np 
+
+def rolling_window(a, window): 
+    """ 
+    Make an ndarray with a rolling window of the last dimension 
+
+    Parameters 
+    ---------- 
+    a : array_like 
+        Array to add rolling window to 
+    window : int 
+        Size of rolling window 
+
+    Returns 
+    ------- 
+    Array that is a view of the original array with a added dimension 
+    of size w. 
+
+    Examples 
+    -------- 
+    >>> x=np.arange(10).reshape((2,5)) 
+    >>> rolling_window(x, 3) 
+    array([[[0, 1, 2], [1, 2, 3], [2, 3, 4]], 
+           [[5, 6, 7], [6, 7, 8], [7, 8, 9]]]) 
+
+    Calculate rolling mean of last dimension: 
+    >>> np.mean(rolling_window(x, 3), -1) 
+    array([[ 1.,  2.,  3.], 
+           [ 6.,  7.,  8.]]) 
+
+    """ 
+    if window < 1: 
+        raise( ValueError, "`window` must be at least 1.") 
+    if window > a.shape[-1]: 
+        raise (ValueError, "`window` is too long.") 
+    shape = a.shape[:-1] + (a.shape[-1] - window + 1, window) 
+    strides = a.strides + (a.strides[-1],) 
+    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides) 
 
 #The experimental values to b matrix are obtained above for the rollwing window
 zz=rolling_window(b1[80:180]*60, 3) #103X48 matrice, i.e 103 data measurments per whole 48 time space
